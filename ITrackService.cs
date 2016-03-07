c8bdeefa0b1b231da0ebace182e3b0998f463ab0
WHOISNVA <kennnaoji@gmail.com>
2016-03-07 01:32:54 -0300

    Added files via upload

---
+++ ITrackService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Sabio.Web.Domain;
+using Sabio.Web.Models.Requests.Tracks;
+
+namespace Sabio.Web.Services
+{
+    public interface ITrackService
+    {
+        void Delete(int id);
+        List<Track> GetAll();
+        Track GetById(int id);
+        int Insert(TracksAddRequest model);
+        void Update(TracksEditRequest model, int id);
+    }
+}
---
+++ TrackViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Sabio.Web.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sabio.Web.Models.ViewModels
+{
+    public class TrackViewModel : BaseViewModel
+    {
+        public int Id { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+}
